7e1715b9f92831f7806549867dc7afcabb425c14
gizatt <gizatt@mit.edu>
2017-08-25 16:02:17 -0400

    Remove DRC-specific pathing

--- python/pyrobotiqhand/robotiq_tcp_node.py
+++ python/pyrobotiqhand/robotiq_tcp_node.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 
 import os,sys
-home_dir = os.getenv("DRC_BASE")
-#print home_dir
-sys.path.append(home_dir + "/software/build/lib/python2.7/site-packages")
-sys.path.append(home_dir + "/software/build/lib/python2.7/dist-packages")
 
 import lcm
 import select
